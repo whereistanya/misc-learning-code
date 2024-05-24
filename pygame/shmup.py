0ba0aa879a258a5b4d232a2ca640ab1a2f6ebdf6
Tanya Reilly <tanya@noidea.dog>
2024-05-24 14:59:57 -0600

    moving paddle

--- pygame/shmup.py
+++ pygame/shmup.py
@@ -13,8 +13,18 @@ class Player(pygame.sprite.Sprite):
     self.rect.bottom = HEIGHT - 10
     self.speedx = 0
 
-    def update (self):
+  def update (self):
+       self.speedx = 0
+       keystate = pygame.key.get_pressed()
+       if keystate[pygame.K_LEFT]:
+         self.speedx = -8
+       if keystate[pygame.K_RIGHT]:
+         self.speedx = 8
        self.rect.x += self.speedx
+       if self.rect.right > WIDTH:
+         self.rect.right = WIDTH
+       if self.rect.left < 0:
+         self.rect.left = 0
 
 
 
@@ -33,7 +43,7 @@ BLUE = (0, 0, 255)
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("The Grand Shmuppenation")
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
